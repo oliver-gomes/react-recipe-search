1b9dcd720f7350c8d159c4779717fee1d149ab0d
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-20 01:10:07 -0400

    name attribute added

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = props => {
   return (
     <form onSubmit={props.getRecipe}>
-      <input type="text" />
+      <input type="text" name="recipeName" />
       <button>Search</button>
     </form>
   );
