c6a47333c23ae1392aa2e351684cf51f7c478e22
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-20 01:02:32 -0400

    getRecipe function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Form from "./components/Form";
 
 class App extends Component {
+  getRecipe = e => {
+    e.preventDefault();
+    console.log("Working");
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
