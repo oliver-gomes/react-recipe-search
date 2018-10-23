2f4e5ff774686d3a4e5db65cec74a893dfaf841e
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 00:00:08 -0400

    new API implement Old one reached limit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
 
-const API_KEY = "a0fca3677f15c289d6edbdae20a60f6c";
+// const API_KEY_f2f = "a0fca3677f15c289d6edbdae20a60f6c";
+
+// `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+
+const API_KEY_ED = "7012144cc2fedba016072774b6cc64dd";
+
+const APP_ID = "44f58ae1";
 
 class App extends Component {
   state = {
@@ -14,11 +20,12 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+      `https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${API_KEY_ED}`
     );
     const data = await api_call.json();
+    console.log(data);
     this.setState({
-      recipes: data.recipes
+      recipes: data.hits
     });
     console.log(this.state.recipes);
   };
