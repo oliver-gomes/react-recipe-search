5571eb2d12ec1a4651a7e55be9f94d2a1f57230f
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-22 20:09:21 -0400

    showcase recipe after Form element

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20${recipeName}`
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
     );
     const data = await api_call.json();
     this.setState({
@@ -29,6 +29,14 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map(recipe => {
+          return (
+            <div key={recipe.recipe_id}>
+              <img src={recipe.image_url} alt={recipe.title} />
+              <p>{recipe.title}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
