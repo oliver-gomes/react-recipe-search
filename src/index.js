646096e41b6cb5c8e5c2707bff14b5850a0bf5ab
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 00:08:31 -0400

    Router implement and App taken out

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "./App";
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Router from "./components/Router";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById("root"));
 registerServiceWorker();
