2205f5767de6b41c741b69893c179f867c50bd8e
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 00:08:16 -0400

    route for home and recipe page

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import App from "../App";
+import Recipe from "./Recipe";
 
 const Router = () => (
-    <BrowserRouter>
-        <Route path='' component={} />
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/recipe:id" component={Recipe} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
