bb12dcdb9b8747d1b24ccac9dc25a3cf193dfcac
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 00:00:19 -0400

    update due to new API

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,25 +1,27 @@
 import React from "react";
 
+// import { Link } from "react-router-dom";
+
 const Recipes = props => (
   <div className="container">
     <div className="row">
       {props.recipes.map(recipe => {
         return (
           <div
-            key={recipe.recipe_id}
+            key={recipe.recipe.label}
             className="col-md-4"
             style={{ marginBottom: "2rem" }}
           >
             <div className="recipes__box">
               <img
                 className="recipe__box-img"
-                src={recipe.image_url}
-                alt={recipe.title}
+                src={recipe.recipe.image}
+                alt={recipe.recipe.lable}
               />
               <div className="recipe__text">
-                <h5>{recipe.title}</h5>
-                <p className="recipe__subtitle">
-                  Publisher: <span>{recipe.publisher}</span>
+                <h5 className="recipes__title">{recipe.recipe.label}</h5>
+                <p className="recipes__subtitle">
+                  Publisher: <span>{recipe.recipe.source}</span>
                 </p>
               </div>
               <button className="recipe_buttons">View Recipe</button>
