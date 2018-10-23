29ed650166c556dcf9a3ad6018a9de45a79b8739
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-22 23:59:49 -0400

    Router component added

---
+++ src/components/Router.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
+const Router = () => (
+    <BrowserRouter>
+        <Route path='' component={} />
+    </BrowserRouter>
+)
