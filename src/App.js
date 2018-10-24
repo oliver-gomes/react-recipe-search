e89cc7db1e4097a8f03fd92e4fff5a247da1e3af
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 23:22:43 -0400

    API update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
 
-// const API_KEY_f2f = "a0fca3677f15c289d6edbdae20a60f6c";
+const API_KEY = "a0fca3677f15c289d6edbdae20a60f6c";
 
 // `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
 
-const API_KEY_ED = "7012144cc2fedba016072774b6cc64dd";
+// const API_KEY_ED = "7012144cc2fedba016072774b6cc64dd";
 
-const APP_ID = "44f58ae1";
+// const APP_ID = "44f58ae1";
 
 class App extends Component {
   state = {
@@ -20,14 +20,14 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${API_KEY_ED}`
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
     );
     const data = await api_call.json();
-    console.log(data);
+    // console.log(data);
     this.setState({
-      recipes: data.hits
+      recipes: data.recipes
     });
-    console.log(this.state.recipes);
+    // console.log(this.state.recipes);
   };
 
   render() {
