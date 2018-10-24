779b55ca27a3c22978ff7525d93566cad37b4374
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-23 23:22:18 -0400

    display the linked recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,52 @@
 import React from "react";
 
-const Recipe = () => {
-  return <div>Recipe Component</div>;
-};
+import { Link } from "react-router-dom";
+
+const API_KEY = "a0fca3677f15c289d6edbdae20a60f6c";
+
+class Recipe extends React.Component {
+  state = {
+    activeRecipe: []
+  };
+
+  componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`
+    );
+    const res = await req.json();
+    this.setState({ activeRecipe: res.recipes[0] });
+  };
+
+  render() {
+    const recipe = this.state.activeRecipe;
+    return (
+      <div className="container">
+        {this.state.activeRecipe.length !== 0 && (
+          <div className="active-recipe">
+            <img
+              className="active-recipe__img"
+              src={recipe.image_url}
+              alt={recipe.title}
+            />
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher: <span>{recipe.publisher}</span>
+            </h4>
+            <p className="active-recipe__website">
+              Website:{" "}
+              <span>
+                <a href={recipe.publisher_url}>{recipe.publisher_url}</a>
+              </span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/"> Go Home</Link>
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Recipe;
